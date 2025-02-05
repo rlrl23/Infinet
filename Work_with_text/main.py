9f4d912fc243eb4773ae0e56ae2a928bacfac3e3
liubov <l.ilinykh@samolet.ru>
2025-02-05 16:32:52 +0500

    Pre-commit

--- Work_with_text/main.py
+++ Work_with_text/main.py
@@ -5,13 +5,11 @@ from tabulate import tabulate
 template = "template.txt"
 output_file = sys.argv[1]
 
-with open(template) as f,open(output_file) as output:
+with open(template) as f, open(output_file) as output:
     re_table = textfsm.TextFSM(f)
     header = re_table.header
     result = re_table.ParseText(output.read())
 
-    print(dict(zip(header, map(lambda x: int(x) if x.isdigit() else x, *result))))
+    print(dict(zip(header, map(lambda x: int(x) if x.isdigit() else x,
+                               *result))))
     print(tabulate(result, headers=header))
-
-
-
